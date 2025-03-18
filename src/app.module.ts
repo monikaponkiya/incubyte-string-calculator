d9e12facf38230bcab72376b8106846c43b80631
Monika <monikaponkiya3@gmail.com>
2025-03-18 11:41:44 +0530

    Project initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+// import { CalculatorModule } from './calculator/calculator.module';
+
+@Module({
+  // imports: [CalculatorModule],
+  providers: [],
+})
+export class AppModule {}
